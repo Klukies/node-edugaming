506c80e4048e6264e021baae8fc665c56b5023b7
Cornille Lukas <lukas.cornille@student.howest.be>
2019-04-30 16:29:02 +0200

    sequelize update

--- config/db.config.js
+++ config/db.config.js
@@ -4,7 +4,6 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(env.database, env.user, env.password, {
   host: env.host,
   dialect: env.dialect,
-  operatorsAliases: false,
 
   pool: {
     max: env.max,
